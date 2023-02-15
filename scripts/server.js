bdb6bdf2413ef8598cd8edeb1ef0fa3a8cfbf5b9
Katie Blunck <97479562+katelyndb@users.noreply.github.com>
2023-02-15 09:09:53 -0700

    Installing package stuff

--- scripts/server.js
+++ scripts/server.js
@@ -1,5 +1,6 @@
 
 /*server.js*/
+/*
 var http = require('http');
 var fs = require('fs');
 
@@ -15,20 +16,18 @@ fs.readFile('../index.html', function (err, html) {
         response.end();  
     }).listen(PORT);
 });
-/*
+*/
+
 const express = require('express')
-const hbs = require('hbs')
-const routes = require('./routes/routes')
 const path = require('path')
 const app = express()
 const PORT = 3000
 
-app.set('view engine', hbs)
+
 app.use('/', routes)
-app.use(express.static(path.join(__dirname, '/public')))
 
 app.listen(PORT, () => {
   console.log(`app is running on PORT ${PORT}`)
 })
 module.exports = app
-*/
+
